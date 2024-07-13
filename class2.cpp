b1bf90042aed3d9c6bccd3665feb8df2eae50124
RumethAmare <160486925+RumethAmare@users.noreply.github.com>
2024-07-13 20:12:10 +0530

    Add files via upload

---
+++ class2.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+class Student
+{
+private:
+    int marks;
+    float average;
+public:
+    void getmarks(int no);
+    float getavg();
+};
+
+void Student::getmarks(int marks)
+{
+    marks = no;
+}
+
+float Student::getavg();
+{
+    return marks / 
+}
+
+
+int main()
+{
+
+    return 0;
+}
